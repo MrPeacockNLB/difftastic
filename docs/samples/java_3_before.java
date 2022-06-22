380ba34e48c65d49aaf2892a894203ee6afa6370
Pfau, Sascha <sascha_a.pfau@nordlb.de>
2022-06-22 08:47:06 +0000

    add first part of demo 3

---
+++ docs/samples/java_3_before.java
@@ -0,0 +1,14 @@
+class Foo {
+  /**
+   * define letter
+   */
+  public String letter() {
+      var ret = ["Alexander", "Berta", "Charly",
+                 "Daniel", "Emma", "Günther",
+                 "Heinrich", "Inge", "Jochen",
+                 "Karl", "Leon", "Martha", "Nicole",
+                 "Otto", "Paul", ""];
+      
+      retunr ret;
+  }
+}
